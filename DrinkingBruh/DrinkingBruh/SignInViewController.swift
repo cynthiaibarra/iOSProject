e852f7d40ecff506f25e32167dc11112cec32459
Cynthia Ibarra <cibarra@utexas.edu>
2017-03-02 18:09:05 -0600

    Firebase authentication state change handler added to signinviewcontroller

---
+++ DrinkingBruh/DrinkingBruh/SignInViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SignInViewController.swift
+//  DrinkingBruh
+//
+//  Created by Cynthia  Ibarra on 3/2/17.
+//  Copyright © 2017 Cynthia  Ibarra. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+    
+    var handler:FIRAuthStateDidChangeListenerHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        handler = FIRAuth.auth()?.addStateDidChangeListener() { (auth, user) in
+            print("=================================")
+            if user != nil {
+                print("Already authenticated.")
+            } else {
+                print("Not authenticated.")
+            }
+            print("=================================")
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // Follow methods viewWillAppear and viewWillDisappear make sure that the navigation bar
+    // does not appear on the SignInViewController screen but does appear for the rest
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
