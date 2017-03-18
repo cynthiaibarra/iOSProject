6a4294f8db0ce1afefc48bdb29a412df5506dc21
Cynthia Ibarra <cibarra@utexas.edu>
2017-03-18 14:51:53 -0500

    added sign in with password/email. fixes #11

--- DrinkingBruh/DrinkingBruh/SignInViewController.swift
+++ DrinkingBruh/DrinkingBruh/SignInViewController.swift
@@ -12,6 +12,10 @@ import FirebaseAuth
 
 class SignInViewController: UIViewController {
     
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     var handler:FIRAuthStateDidChangeListenerHandle?
     
     override func viewDidLoad() {
@@ -21,13 +25,12 @@ class SignInViewController: UIViewController {
         handler = FIRAuth.auth()?.addStateDidChangeListener() { (auth, user) in
             print("=================================")
             if user != nil {
-                print("Already authenticated.")
+                print("Authenticated.")
                 print(user?.email ?? "err")
-                    self.performSegue(withIdentifier: "segueToHome", sender: nil)
+                self.performSegue(withIdentifier: "segueToHome", sender: nil)
            } else {
                 print("Not authenticated.")
            }
-            print("=================================")
             
         }
     }
@@ -41,6 +44,27 @@ class SignInViewController: UIViewController {
     
     // Follow methods viewWillAppear and viewWillDisappear make sure that the navigation bar
     // does not appear on the SignInViewController screen but does appear for the rest
+    @IBAction func signInButton(_ sender: Any) {
+        messageLabel.text = ""
+        let email:String = emailTextField.text!
+        let password:String = passwordTextField.text!
+        
+        if email.isEmpty || password.isEmpty {
+            messageLabel.text = "Must fill in all entries."
+        } else {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+                if error != nil {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                
+            }
+        }
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
