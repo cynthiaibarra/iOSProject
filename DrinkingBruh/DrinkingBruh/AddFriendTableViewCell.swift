46a275339fffb7b133cbf0a9aa6dc836151dc1d5
Cynthia Ibarra <cibarra@utexas.edu>
2017-03-23 14:37:59 -0500

    friend system milestone: sent friend request done

---
+++ DrinkingBruh/DrinkingBruh/AddFriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddFriendTableViewCell.swift
+//  DrinkingBruh
+//
+//  Created by Cynthia  Ibarra on 3/23/17.
+//  Copyright © 2017 Cynthia  Ibarra. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
