2f43dbcb56548bb4634aa14590ed1543c1dd6469
Vineeth K <vineeth@utexas.edu>
2017-03-24 00:59:49 -0500

    updating branch

--- DrinkingBruh/DrinkingBruh/SettingsTableViewController.swift
+++ DrinkingBruh/DrinkingBruh/SettingsTableViewController.swift
@@ -9,87 +9,92 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var drunkModeLabel: UILabel!
+    @IBOutlet weak var themeLabel: UILabel!
+    @IBOutlet weak var drunkModeSwitch: UISwitch!
+    @IBOutlet weak var themeSegControl: UISegmentedControl!
+    
+    var themeDict:[String:UIColor]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        self.title = "Settings"
+        
+        //Get current theme and set theme for the view
+        //and sub views and set state for themeSegControl
+        
+        self.themeDict = Theme.getTheme()
+        drunkModeLabel.textColor = themeDict?["textColor"]
+        themeLabel.textColor = themeDict?["textColor"]
+        self.view.backgroundColor = themeDict?["viewColor"]
+        
+        let theme:String = Config.theme()
+        
+        if theme == "light" {
+            themeSegControl.selectedSegmentIndex = 0
+        }
+        else {
+            themeSegControl.selectedSegmentIndex = 1
+        }
+        
+        //Get drunk mode state and set drunkModeSwitch
+        
+        let dMode:String = Config.drunkMode()
+        
+        if dMode == "on" {
+            drunkModeSwitch.setOn(true, animated: true)
+        }
+        else {
+            drunkModeSwitch.setOn(false, animated: true)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    //MARK: Actions
+    
+    @IBAction func editProfileButton(_ sender: Any) {
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @IBAction func drunkModeAction(_ sender: Any) {
+        
+        if let dMode = sender as? UISwitch {
+            
+            if dMode.isOn {
+                Config.setDrunkMode("on")
+            }
+            else {
+                Config.setDrunkMode("off")
+            }
+        }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    
+    @IBAction func themeSegControlAction(_ sender: Any) {
+        
+        //Set theme to light or dark in UserDefaults and update view colors
+        
+        if let theme = sender as? UISegmentedControl {
+            
+            if theme.selectedSegmentIndex == 0 {
+                Config.setTheme("light")
+            }
+            else {
+                Config.setTheme("dark")
+            }
+        }
+        
+        self.themeDict = Theme.getTheme()
+        drunkModeLabel.textColor = themeDict?["textColor"]
+        themeLabel.textColor = themeDict?["textColor"]
+        self.view.backgroundColor = themeDict?["viewColor"]
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
