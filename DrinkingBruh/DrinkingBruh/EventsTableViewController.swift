bb243b74692610f5c8945966faf150fab8191e64
Cynthia Ibarra <cibarra@utexas.edu>
2017-03-30 17:22:54 -0500

    laid down ground work for events.

---
+++ DrinkingBruh/DrinkingBruh/EventsTableViewController.swift
@@ -0,0 +1,109 @@
+//
+//  EventsTableViewController.swift
+//  DrinkingBruh
+//
+//  Created by Cynthia  Ibarra on 3/30/17.
+//  Copyright © 2017 Cynthia  Ibarra. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewController: UITableViewController {
+    
+    private let myEvents:[[String:Any]] = []
+    private let invitedEvents:[[String:Any]] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let addEventButton:UIButton = UIButton.init(type: UIButtonType.custom)
+        addEventButton.setImage(UIImage(named: "plus.png"), for: .normal)
+        addEventButton.addTarget(self, action: #selector(self.segueToCreateEvent), for: .touchUpInside)
+        addEventButton.frame = CGRect.init(x: 0, y: 0, width: 30, height: 30)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: addEventButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if section == 0 {
+            return invitedEvents.count
+        }
+        return myEvents.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath)
+        
+        var event:[String:Any] = [:]
+        if indexPath.section == 0 {
+            event = invitedEvents[indexPath.row]
+        } else {
+            event = myEvents[indexPath.row]
+        }
+        // Configure the cell...
+
+        return cell
+    }
+    
+    @objc private func segueToCreateEvent() {
+        performSegue(withIdentifier: "segueToCreateEvent" , sender: self)
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
