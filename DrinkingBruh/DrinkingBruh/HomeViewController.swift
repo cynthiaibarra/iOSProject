49af5b889cd12730d0ed2de44fa69cd2c08dd510
Cynthia Ibarra <cibarra@utexas.edu>
2017-04-10 19:53:41 -0500

    sign out button added to side menu

--- DrinkingBruh/DrinkingBruh/HomeViewController.swift
+++ DrinkingBruh/DrinkingBruh/HomeViewController.swift
@@ -20,8 +20,6 @@ class HomeViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-        
         // Dispose of any resources that can be recreated.
     }
 
